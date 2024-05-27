5586047a81195cc333c26ad4517290cc4dcda0b9
m.hryshko <chumakova.marina@gmail.com>
2024-05-27 10:54:51 +0300

    Homework 46. Added user login and registration page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+from accounts.managers import CustomUserManager
+from accounts.validators import validate_username, validate_name
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=30, unique=True, validators=[validate_username]
+    )
+    email = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=50, validators=[validate_name])
+    last_name = models.CharField(max_length=50, validators=[validate_name])
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_query_name='user',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='user',
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        self.first_name = self.first_name.capitalize()
+        self.last_name = self.last_name.capitalize()
+        super().save(*args, **kwargs)
